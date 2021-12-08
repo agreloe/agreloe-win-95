ca0491ee2bab948100bdb3cbc1df9235cc6878b9
agreloe <agreloerica@gmail.com>
2021-12-08 19:13:37 -0300

    fix(App.css): First setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  
+  
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <mark>src/App.js</mark> and save to reload.
         </p>
+
+        <button className="button-pink">Click here</button>
+
+        <button>Click here</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +28,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+    <label>Please identify yourself:</label>
+    <input value={input} onInput={e => setInput(e.target.value)}/>
+    <p>Hello {input}</p>
+    
+    </div>
     </div>
   );
 }
